9a90b081e0922abe10e0b83d47f86a72aaf5941d
sebasdeldi <sebasdeldi123@gmail.com>
2016-04-14 10:08:23 -0500

    modals behaviour working correctly

--- app/assets/javascripts/show_artist_profile.js
+++ app/assets/javascripts/show_artist_profile.js
@@ -1,9 +1,25 @@
 $( document ).ajaxComplete(function() {
 
-	$('.card.custom').click(function(){
-	  $('.ui.modal').modal('show');
+	// $('.card.custom').click(function(){
+	//   $('.ui.modal').modal('show');
 
+	// });
+
+	$('.card.custom').click(function(event){
+		var card_clicked = $(this).attr('data-id');
+
+		$('#modal-' + card_clicked).modal('show');
 	});
+	
 
- 	// alert('ajax finished');
+
+
+
+
+		// for (var i = 0; i < 19; i++) {
+		// 	if (card_clicked === i) {
+		// 		$('#modal-0').modal('show');
+		// 	}
+		// };
+
 });
