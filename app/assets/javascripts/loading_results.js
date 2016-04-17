57346285bd279d3540004d375d982f333218c888
sebasdeldi <sebasdeldi123@gmail.com>
2016-04-17 12:26:33 -0500

    styling

--- app/assets/javascripts/loading_results.js
+++ app/assets/javascripts/loading_results.js
@@ -5,11 +5,15 @@ $( document ).ajaxStart(function() {
 	$('.ui.text.container.main i').transition('scale');
 
   $('.ui.inline.loader').addClass('active');
+  $('.loader-msg').prepend('<br>')
+  $('.loader-msg').append('<p><span>Our</span> <span class="my_blue"><b>Melody Genie</b></span> <span>is looking for the best artists for you!</span></p>');
   // alert('ajax started');
 });
 
 $( document ).ajaxComplete(function() {
 	$('.ui.inline.loader').removeClass('active');
+	$('.loader-msg').css('display', 'none');
+	
 
 
  	// alert('ajax finished');
