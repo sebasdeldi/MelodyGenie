7af1601ac5fe93278895dcd890a2cf730d44c184
sebasdeldi <sebasdeldi123@gmail.com>
2016-08-16 12:35:13 -0500

    jjjj

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,9 @@ class PagesController < ApplicationController
 
 	def info
 		if params[:initzarqkr].present?
-		  @h1 = params[:h]
-		  @a1 = params[:a]
+			@init = params[:initzarqkr]
+		  	@h1 = params[:h]
+		  	@a1 = params[:a]
 		end
 
 		if params[:trigger].present?
